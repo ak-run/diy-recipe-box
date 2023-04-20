72f03c155375adf74caed0f1e91681d4c13e8114
ak-run <123417735+ak-run@users.noreply.github.com>
2023-04-20 16:28:34 +0100

    added menu items and footer component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+//to link the pages, importing from react-router-dom
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import ShoppingList from "./ShoppingList";
+import RecipeFinder from "./RecipeFinder";
+// import Hero from "./components/heroBanner";
+import Footer from "./components/Footer";
+import Card from "./components/NavCardGroup";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Router>
+          <div>
+          <header>
+          {/* <Hero /> */}
+          </header>
+            {/*using Routes to link between pages */}
+           <Routes>
+              <Route path="/ShoppingList" exact element={<ShoppingList />} />
+              <Route path="/RecipeFinder" exact element={<RecipeFinder />} />
+            </Routes>
+          </div>
+        <Card />
+        </Router>
+      </div>
+      <Footer/>
+    </>
   );
 }
-
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Components.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-info">
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
