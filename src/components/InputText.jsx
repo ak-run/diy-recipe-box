71915fd51cf7358f8cb37a6fea68fa98cccb9be9
ak-run <123417735+ak-run@users.noreply.github.com>
2023-04-27 20:29:06 +0100

    added add button and focus on input after adding new item to shopp ing list

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 //component to take user input and add items to the shopping list
 const InputText = (props) => {
   const [value, setValue] = useState('');
+  
+  const inputRef = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (value.trim() !== '') { // Check if value is not empty or whitespace
       props.handleSubmit(value);
       setValue('');
+      //after clicking the button the focus goes to input that has a ref inputRef
+      inputRef.current.focus();
     }
   };
+
 //returning a form containing a text input and a submit button with an onSubmit event that calls handleSubmit function with the current value of the input as argument
   return (
     <div className='AddItemBox'>
@@ -21,8 +26,16 @@ const InputText = (props) => {
         placeholder="Add an Item"
         value={value} 
         onChange={e => setValue(e.target.value)}
+        ref={inputRef}
       />
     </form>
+    <button 
+      onClick={handleSubmit}
+      value={value} 
+      onChange={e => setValue(e.target.value)}
+      >
+      Add
+      </button>
     </div>
   );
 };
